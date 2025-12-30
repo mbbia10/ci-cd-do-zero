e3a462d64a70673d1a113826cd10ae8c143b02b4
KenzoaokiTbxtech <kenzo.aoki@tbxtech.com>
2025-12-30 09:31:23 -0300

    Adição de novas funcionalidades

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
-const soma = require("./app");
+import { soma, subtrai } from "./app.js";
 
-if (soma(2, 2) !== 4) {
-  throw new Error("Teste falhou ❌");
-}
 
-console.log("Teste passou ✅");
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+
+
+const numero1 = parseFloat(prompt("Digite o primeiro número: "));
+const numero2 = parseFloat(prompt("Digite o segundo número: "));
+
+const resultado_soma = soma(numero1, numero2);
+const resultado_subtrai = subtrai(numero1, numero2);
+
+console.log(`Soma de ${numero1} + ${numero2} = ${resultado_soma}`);
+console.log(`Subtração de ${numero1} - ${numero2} = ${resultado_subtrai}`);
+
